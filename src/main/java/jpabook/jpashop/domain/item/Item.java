9b4f5a1b56e4d8068872e04e5bb9a0f2460f1426
tngh4037 <tngh4037@gmail.com>
2024-05-13 07:20:07 +0900

    section2: 도매인 분석 설계

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.InheritanceType.SINGLE_TABLE;
+
+@Entity
+@Getter @Setter
+@Inheritance(strategy = SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+}

088bfd831cb11e7e22c279424ab2163a1292e1d3
tngh4037 <tngh4037@gmail.com>
2024-05-16 05:56:45 +0900

    활용2) section3: API 개발 고급 - 지연 로딩과 조회 성능 최적화 - V2: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xxxToOne (ManyToOne, OneToOne) 에서의 성능 최적화
@@ -31,6 +36,41 @@ public class OrderSimpleApiController {
         }
 
         return all;
+    }
+
+    /**
+     * v2. 엔티티를 조회해서 dto로 변환 (fetch join 사용 x)
+     * - 단점: 지연로딩으로 쿼리 N번 호출
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() { // 참고) 참고로 리스트 자체가 아니라 한번 더 감싸서 반환하자. (여기서는 거기까지는 생략)
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        // [ occurred N+1 ]
+        // - order 조회 1번
+        // - (주문마다 loop를 돌면서) member 조회, delivery 조회
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) { // 참고) DTO 에서 엔티티를 파라미터로 받는 것은 크게 문제가 되지 않는다.
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY
+        }
     }
 
 }
